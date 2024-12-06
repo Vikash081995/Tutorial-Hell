404c7f43725f37eaf39c846f258954678f5c9980
Vikash kumar <vikash08.kumar1995@gmail.com>
2024-12-06 16:38:20 +0530

    testing list of elements

---
+++ learning-react-testing-library/src/components/skills/skills.tsx
@@ -0,0 +1,14 @@
+import { SkillsProps } from "./skills.types";
+
+export const Skills = ({ skills }: SkillsProps) => {
+  return (
+    <>
+      <ul>
+        {skills.map((skill) => {
+          return <li key={skill}>{skill}</li>;
+        })}
+      </ul>
+    </>
+  );
+};
+    
