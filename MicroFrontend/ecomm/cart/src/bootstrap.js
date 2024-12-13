e94c4e64153acd8de3357cb9a6d1f1a4d58e9980
Vikash kumar <vikash08.kumar1995@gmail.com>
2024-12-13 22:11:02 +0530

    sharing dependencies

---
+++ MicroFrontend/ecomm/cart/src/bootstrap.js
@@ -0,0 +1,4 @@
+import faker from "faker";
+
+const cartItem = `<div>${faker.commerce.productName()}</div>`;
+document.querySelector("#cart-dev").innerHTML = cartItem;
--- MicroFrontend/ecomm/cart/src/index.js
+++ MicroFrontend/ecomm/cart/src/index.js
@@ -1,5 +1,1 @@
-import faker from 'faker';
-
-
-const cartItem =`<div>${faker.commerce.productName()}</div>`;
-document.querySelector("#cart-dev").innerHTML = cartItem;
+import('./bootstrap')
