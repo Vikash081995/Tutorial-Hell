9ac0297b964127947429824354730e436df7e413
Vikash kumar <vikash08.kumar1995@gmail.com>
2024-12-15 23:37:03 +0530

    mfe-project2-setup

---
+++ Stephen-Grider/MicroFrontend/mfp/packages/marketing/src/bootstrap.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+//Mount function to start up the app
+const mount = (el) => {
+    ReactDOM.render(
+        <h1>Hi There</h1>,el
+    );
+}
+
+
+//If we are in development and is in isolation 
+//call mount immediately
+
+if(process.env.NODE_ENV === 'development'){
+    const devRoot = document.querySelector('#_marketing-dev-root');
+    if(devRoot){
+        mount(devRoot);
+    }
+}
+
+export {mount}
