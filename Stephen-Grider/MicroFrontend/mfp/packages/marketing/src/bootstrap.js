40b3a8449e1f2740dee8c8eccd3cd143cc307d42
Vikash kumar <vikash08.kumar1995@gmail.com>
2024-12-17 00:08:50 +0530

    changed for mfe

--- Stephen-Grider/MicroFrontend/mfp/packages/marketing/src/bootstrap.js
+++ Stephen-Grider/MicroFrontend/mfp/packages/marketing/src/bootstrap.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App"
 
 //Mount function to start up the app
 const mount = (el) => {
-    ReactDOM.render(
-        <h1>Hi There</h1>,el
-    );
+    ReactDOM.render( <App />,el)
 }
-
-
 //If we are in development and is in isolation 
 //call mount immediately
 
